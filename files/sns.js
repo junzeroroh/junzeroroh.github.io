96eb85c05954817c3622a91110d373b15f77c883
junzeroroh <43209003+junzeroroh@users.noreply.github.com>
2022-06-11 15:10:21 +0900

    wedding
    up

---
+++ files/sns.js
@@ -0,0 +1,26 @@
+﻿
+function sendSns(sns, url, txt) {
+    var o;
+    var _url = encodeURIComponent(url);
+    var _txt = encodeURIComponent(txt);
+    var _br = encodeURIComponent('\r\n');
+
+    switch (sns) {
+        case 'facebook':
+            o = {
+                method: 'popup',
+                url: 'http://www.facebook.com/sharer/sharer.php?u=' + _url
+            };
+            break;
+
+        default:
+            alert('지원하지 않는 SNS입니다.');
+            return false;
+    }
+
+    switch (o.method) {
+        case 'popup':
+            window.open(o.url);
+            break;
+    }
+}
